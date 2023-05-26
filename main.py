c7f5bd9fc08c214e72bd4d82191d7a3a2155c5ef
qinanchuan <qinanchuan@pinduoduo.com>
2023-05-26 14:55:50 +0800

    +exploit_db

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 from obj import vuln_obj
 from scan.cnnvd import scan as cnnvd_scan
 from scan.oscs import scan as oscs_scan
+from scan.exploit_db import scan as exploit_scan
 from sql_helper import is_not_exist, insertTo, insertall
 
 # 设置日志级别为DEBUG
@@ -23,7 +24,7 @@ logger.addHandler(console_handler)
 
 async def async_run():
     tasks = []
-    # tasks.append(cnnvd_scan(2))
+    tasks.append(exploit_scan(7))
     tasks.append(oscs_scan(7))
     results_list = await asyncio.gather(*tasks)
 
